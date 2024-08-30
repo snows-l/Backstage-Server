c1059bc91907a0969e8aaf2ef5656f3f28ea214a
snows_l <snows_l@163.com>
2024-08-30 22:39:32 +0800

    perf

--- WebsiteUI/src/utils/request.js
+++ WebsiteUI/src/utils/request.js
@@ -20,7 +20,7 @@ let isDev = import.meta.env.MODE === 'development';
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: isDev ? import.meta.env.VITE_DEV_BASE_URL : import.meta.env.VITE_PROP_BASE_URL,
+  baseURL: isDev ? import.meta.env.VITE_DEV_BASE_URL : import.meta.env.VITE_PROD_BASE_URL,
   // 超时
   timeout: 100000
 });
